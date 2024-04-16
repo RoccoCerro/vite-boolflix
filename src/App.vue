00393e4204b41f44be57facaee707b2d6db3a83f
RoccoCerro <rcerro02@gmail.com>
2024-04-16 12:41:40 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <ContentHeader />
+</template>
+
+<script>
+  import ContentHeader from "./components/ContentHeader.vue";
+
+  export default {
+    components:{
+      ContentHeader
+    }
+  }
+</script>
+
+<style lang="scss">
+  @use './style/general.scss' as *;
+</style>
---
+++ src/components/ContentHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="content-header">
+    <div class="container">
+      <div class="row">
+        <div class="col">LOGO</div>
+        <div class="col">
+          <input type="text" placeholder="Inserisci il nome del film">
+          <button>Cerca</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+  @use '../style/partials/variables';
+
+  .content-header {
+    padding: 20px 0;
+
+    .row {
+      justify-content: space-between;
+    }
+
+    input, button {
+      height: 30px;
+      border: none;
+      border-radius: variables.$radius;
+      padding: 0 5px;
+    }
+
+    input {
+      outline: none;
+      margin-right: 10px;
+    }
+  }
+</style>
