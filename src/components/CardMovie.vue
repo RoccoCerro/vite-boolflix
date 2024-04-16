67e9a687899f7733a187bfe33d61af1fce272e01
RoccoCerro <rcerro02@gmail.com>
2024-04-16 18:48:47 +0200

    Add: star valutation

--- src/components/CardMovie.vue
+++ src/components/CardMovie.vue
@@ -7,18 +7,26 @@
       <li>{{ movie.title }} {{ movie.name }}</li>
       <li>{{ movie.original_title }} {{ movie.original_name}}</li>
       <li v-if="movie.original_language === 'it'">
-        <img src="../assets/italiana.webp" alt="">
+        <img src="../assets/italiana.webp" alt="Bandiera Italiana">
       </li>
       <li v-else>
         le flag non ci sono in FontAwesome
       </li>
-      <li>{{ movie.vote_average }}</li>
+      <li v-html="fullStar"></li>
+      <li>{{ rating }}</li>
     </ul>
   </div>
 </template>
 
 <script>
   export default {
+    data(){
+      return{
+        fullStar:"",
+        rating: null,
+      }
+    },
+
     props: {
       movie: Object,
     },
@@ -26,7 +34,23 @@
     methods:{
       concatUrl(){
         return `https://image.tmdb.org/t/p/w342${this.movie.poster_path}`
+      },
+
+      quantityOfTheSame(){
+        this.rating = Math.round(this.movie.vote_average / 2);
+        console.log(this.rating)
+        for(let i = 1; i <= 5; i++){
+          if(this.rating >= i){
+            this.fullStar += '<span class="color-red"><i class="fa fa-solid fa-heart"></i></span>'
+          }else if(this.rating <= i){
+            this.fullStar += '<i class="fa-solid fa-heart"></i>'
+          }
+        }
       }
+    },
+
+    created(){
+      this.quantityOfTheSame()
     }
   }
 </script>
