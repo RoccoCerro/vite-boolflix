80a197ca1d99f5044aeb47ee61cefe281c21a4d1
RoccoCerro <rcerro02@gmail.com>
2024-04-16 13:46:08 +0200

    Add: value of res in the store

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,17 @@
 <script>
   import ContentHeader from "./components/ContentHeader.vue";
   import axios from 'axios';
+  import {store} from './store.js'
 
   export default {
     components:{
       ContentHeader
+    },
+
+    data(){
+      return{
+        store: store,
+      }
     },
 
     methods:{
@@ -23,8 +30,18 @@
           }
         )
         .then((res)=>{
-          console.log(res)
-          console.log(res.data)
+          console.log('Risultato res', res)
+          console.log('Risultato res.data', res.data)
+          console.log('Risultato res.data.results', res.data.results)
+
+          this.store.results = res.data.results
+          // store.results = res.data.results 
+          this.store.page = res.data.page
+          this.store.total_pages = res.data.total_pages
+          this.store.total_results = res.data.total_results
+
+          console.log('Risultato this.store', this.store)
+          console.log('Risultato store', store)
         })
       }
     },
--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-	
+  page: null,
+	results: [],
+  total_pages: null,
+  total_results: null
 })
