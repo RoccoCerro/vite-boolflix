8f4fcd0d3cbafc5c310f5db639b88c7de42fc48c
RoccoCerro <rcerro02@gmail.com>
2024-04-16 13:04:28 +0200

    Add: call to the server movie

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,33 @@
 
 <script>
   import ContentHeader from "./components/ContentHeader.vue";
+  import axios from 'axios';
 
   export default {
     components:{
       ContentHeader
+    },
+
+    methods:{
+      calledMovieServer(){
+        axios
+          .get(
+            `https://api.themoviedb.org/3/search/movie`,{
+            params: {
+              api_key: "0e75ad5772cce745dda6b939d03ca9de",
+              query: "futuro"
+            }
+          }
+        )
+        .then((res)=>{
+          console.log(res)
+          console.log(res.data)
+        })
+      }
+    },
+
+    created(){
+      this.calledMovieServer()
     }
   }
 </script>
