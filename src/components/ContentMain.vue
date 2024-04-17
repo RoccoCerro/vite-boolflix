5606f295c64c476014d40e260a24bcd9665e1dd2
RoccoCerro <rcerro02@gmail.com>
2024-04-17 14:51:10 +0200

    Add: v-if in the content main in case there are no results

--- src/components/ContentMain.vue
+++ src/components/ContentMain.vue
@@ -8,6 +8,8 @@
         <li v-for="movie in store.tv.results" class="col-3">
           <CardMovie :movie="movie"/>
         </li>
+        <li v-if="store.movie.results.length == 0">NON CI SONO RISULTATI TRA I FILM PER LA RICERCA EFFETTUATA</li>
+        <li v-if="store.tv.results.length == 0">NON CI SONO RISULTATI TRA LE SERIE TV PER LA RICERCA EFFETTUATA</li>
       </ul>
     </div>
   </div>
@@ -22,6 +24,10 @@
       return{
         store: store,
       }
+    },
+
+    methods:{
+    
     },
 
     components:{
