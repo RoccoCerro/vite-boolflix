4b7e712d35bcfd5ef54f760a66e80d85932f6c08
RoccoCerro <rcerro02@gmail.com>
2024-04-17 15:01:16 +0200

    Fix: style li in content main

--- src/components/ContentMain.vue
+++ src/components/ContentMain.vue
@@ -8,8 +8,8 @@
         <li v-for="movie in store.tv.results" class="col-3">
           <CardMovie :movie="movie"/>
         </li>
-        <li v-if="store.movie.results.length == 0">NON CI SONO RISULTATI TRA I FILM PER LA RICERCA EFFETTUATA</li>
-        <li v-if="store.tv.results.length == 0">NON CI SONO RISULTATI TRA LE SERIE TV PER LA RICERCA EFFETTUATA</li>
+        <li class="no-movie" v-if="store.movie.results.length == 0">NON CI SONO RISULTATI TRA I FILM PER LA RICERCA EFFETTUATA</li>
+        <li class="no-tv" v-if="store.tv.results.length == 0">NON CI SONO RISULTATI TRA LE SERIE TV PER LA RICERCA EFFETTUATA</li>
       </ul>
     </div>
   </div>
@@ -43,8 +43,12 @@
 <style lang="scss" scoped>
   .content-main{
 
-    li:nth-last-child(2), li:last-of-type{
+    li.no-movie,li.no-tv{
       background-color: rgba(255, 255, 255, 0.193);
+      margin-left: 15px;
+      margin-top: 5px;
+      padding: 5px 10px;
+      border-radius: 5px;
     }
   }
 
